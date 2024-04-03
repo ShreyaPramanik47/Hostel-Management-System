dbe63043d659e2b2039fc863a48ea095b7ba1904
ShreyaPramanik47 <shreyapramanik909@gmail.com>
2024-04-04 02:09:38 +0530

    Added body section, updated Html , CSS & js

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-$(window).scroll(function(){
-    if($(this).scrollTop() > 100){
-        $('.nav').addClass('sticky')
-    } else{
-        $('.nav').removeClass('sticky')
-    }
-  });
+// $(window).scroll(function(){
+//     if($(this).scrollTop() > 100){
+//         $('.nav').addClass('sticky')
+//     } else{
+//         $('.nav').removeClass('sticky')
+//     }
+//   });
+
+// Select the navbar element
+// Select the navbar element
+const navbar = document.querySelector('.nav');
+
+// Function to add the 'sticky' class to the navbar when scrolling
+function stickyNavbar() {
+  if (window.scrollY > 0) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  }
+}
+
+// Add scroll event listener
+window.addEventListener('scroll', stickyNavbar);
